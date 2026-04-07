14052de9ba7a92211d340e9d2c74b356537f579a
skranz-auto <xsx50@uni-ulm.de>
2026-04-07 08:29:02 +0000

    Update to gha_test

--- install_repbox_pkgs.R
+++ install_repbox_pkgs.R
@@ -1,6 +1,6 @@
 cat("\nInstall local R packages\n")
 #remotes::install_local("pkgs/GithubActions")
-#remotes::install_local("pkgs/repboxEJD")
+remotes::install_local("pkgs/repboxStata", force=TRUE)
 remotes::install_local("pkgs/repboxRun",force=TRUE)
 
 cat("\nDone local package installation.\n")
