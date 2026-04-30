763cd069f11f587963bae8f7209fb8c4d1b24b0f
skranz-auto <xsx50@uni-ulm.de>
2026-04-30 05:37:49 +0000

    Update to aejapp_1_3_4

--- install_repbox_pkgs.R
+++ install_repbox_pkgs.R
@@ -1,11 +1,15 @@
 cat("\nInstall local R packages\n")
 install.packages("remotes")
 inst = function(pkg, force=TRUE) {
+  cat("\nInstall local ", pkg,"\n")
   suppressPackageStartupMessages(remotes::install_local(pkg, force=force))
 
 }
-inst("pkgs/repboxStata")
-inst("pkgs/repboxRun")
-inst("pkgs/repboxUtils")
+pkgs = basename(list.dirs("pkgs", full.names=FALSE, recursive = FALSE))
+for (pkg in pkgs) inst(paste0("pkgs/", pkg))
+
+#inst("pkgs/repboxStata")
+#inst("pkgs/repboxRun")
+#inst("pkgs/repboxUtils")
 
 cat("\nDone local package installation.\n")
