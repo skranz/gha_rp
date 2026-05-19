344ea3e0329ed447b6462438320709546c0f5017
skranz-auto <xsx50@uni-ulm.de>
2026-05-19 08:05:22 +0000

    Update to aeri_5_4_8

--- pkgs/repboxRun/R/load_repbox_libs.R
+++
@@ -1,7 +0,0 @@
-repbox_load_libs = function() {
-  libs = c("repboxRun", "repboxDB", "repboxUtils", "repboxRegmap", "repboxStata", "repboxStataReg","repboxCodeText", "repboxR",         "repboxDoc", "repboxDRF", "metaregBase", "repboxEJD")
-
-  for (lib in libs) {
-    suppressPackageStartupMessages(require(lib,character.only = TRUE,warn.conflicts = FALSE))
-  }
-}
